9fc8f3c1e7ac13296080676f2d13fa946e311230
Muhammad Faizan <mfaizan2570@gmail.com>
2026-07-08 10:57:36 +0100

    Update and rename ddl_silver to ddl_silver.sql

--- Scripts/Silver/ddl_silver.sql
+++ Scripts/Silver/ddl_silver.sql
@@ -13,6 +13,7 @@ Usage:
 ===============================================================================
 */
 
+
 IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
 	DROP TABLE silver.crm_cust_info
 CREATE TABLE silver.crm_cust_info (
